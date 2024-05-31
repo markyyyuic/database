0a8c21d853d7dcee37d096c418547a50f398a398
markyyyuic <mnisnea_220000000419@uic.edu.ph>
2024-05-31 21:14:46 +0800

    sdsa
    Co-Authored-By: jcondee <142201907+jcondee@users.noreply.github.com>

--- db/database.py
+++ db/database.py
@@ -5,7 +5,7 @@ db_config = {
     "host": "localhost",
     "user": "root",
     "password": "",
-    "database": "scsaddsadshedule",
+    "database": "scsaddsadssadsahedule",
     "port": 3306,
 }
 
